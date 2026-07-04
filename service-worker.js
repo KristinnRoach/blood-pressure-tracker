fa50d94696e1e7a1788da67f092ecfec6f7b258c
kiddi <kristinnroach@gmail.com>
2026-07-04 01:46:20 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Blood Pressure Tracker Service Worker
 // Simple cache-first strategy for offline functionality
 
-const CACHE_NAME = 'bp-tracker-1783128556985';
+const CACHE_NAME = 'bp-tracker-1783129579520';
 const STATIC_ASSETS = [
   './',
   './index.html',
